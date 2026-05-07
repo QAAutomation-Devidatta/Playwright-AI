99ea5c4a7046f3a1d3aa5e5a129d5a8922ac5924
Devidatta <devidatta.automation@example.com>
2026-05-07 23:48:21 +0530

    Initial commit: Add Playwright AI learning materials

---
+++ Chapter_01_Basics/01_Basics.js
@@ -0,0 +1,11 @@
+console.log("Hello, World!");
+//first program in Node.js
+
+console.log(process.platform);
+//to know the operating system platform
+
+console.log(process.arch);
+//to know the architecture of the system
+
+console.log(process.version);
+//to know the Node.js versions
